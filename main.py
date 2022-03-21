8f2e8c3cc2caba435c737f38cf662f5d9b247155
Rahmatullin <65865859+Aydar8888@users.noreply.github.com>
2022-03-21 19:50:36 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+page = 1
+while True:
+    url = "https://stopgame.ru/games/filter?rating=izumitelno&p=" + str(page)
+    src = requests.get(url)
+    soup = BeautifulSoup(src.text, "lxml")
+    game_list = soup.findAll('div', class_="caption caption-bold")
+    gage_icon = soup.find_all('div', class_="image slanted")
+    for a in gage_icon:
+        icon_url = a.get("style")
+    if (len(game_list)):
+        page += 1
+        for date in game_list:
+            c = date.find('a')
+            item_url = "https://stopgame.ru" + c.get("href")
+            print(f"{date.text.strip()}: {item_url}\n{icon_url}") 
+        
+            
+        if page == 4:
+            break
+    
+    
+    
+    
